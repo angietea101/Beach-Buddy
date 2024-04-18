35f0925da58c16cb48e9e0f82e0015779f4304d2
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2024-04-18 01:46:00 -0700

    fixed error

--- htmlScrape.py
+++ htmlScrape.py
@@ -25,31 +25,4 @@ for course_block in course_blocks:
             # Print the row data in the specified format
             print('\t'.join(row_data))
         print()  # Print an extra line between tables
-    print()  # Print an extra line between course blocksfrom bs4 import BeautifulSoup
-import requests
-url = 'http://web.csulb.edu/depts/enrollment/registration/class_schedule/Fall_2024/By_Subject/CECS.html'
-response = requests.get(url)
-html_content = response.text
-
-soup = BeautifulSoup(response.text, 'html.parser')
-course_blocks = soup.find_all('div', class_='courseBlock')
-for course_block in course_blocks:
-    course_code = course_block.find('span', class_='courseCode').text
-    course_title = course_block.find('span', class_='courseTitle').text
-    units = course_block.find('span', class_='units').text
-    print("Course Code:", course_code)
-    print("Course Title:", course_title)
-    print("Units:", units)
-
-    # Find all tables within the current course block
-    tables = course_block.find_all('table', class_='sectionTable')
-    for table in tables:
-        rows = table.find_all('tr')
-        for row in rows:
-            cells = row.find_all('td')
-            # Extract and organize data from each row
-            row_data = [cell.text.strip() for cell in cells]
-            # Print the row data in the specified format
-            print('\t'.join(row_data))
-        print()  # Print an extra line between tables
-    print()  # Print an extra line between course blocks
+    print()  # Print an extra line between course blocks from bs4 import BeautifulSoup
