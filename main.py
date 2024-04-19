837e640bd0ec5aeb62867d7ac8ae69f96fd4c7a6
Angie <angietea101@gmail.com>
2024-04-18 17:37:02 -0700

    REFACTOR: Create scrape function into a function and conventions

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from CSULB_course import CSULBCourse
-def main():
-    courses = []
+"""
+File: main.py
+Author: Angie Tran and Diego Cid
+Description: Main function to run our script
+"""
+from csulb_course import CSULBCourse
+
+
+def scrape_data_to_file():
     with open('scraped_data.txt', 'r') as file:
+        courses = []
         for line in file:
             data = line.strip().split(', ')
             course_abr = data[0]
@@ -18,14 +25,20 @@ def main():
             instructor = data[13]
             comment = data[14]
             courses.append(
-                CSULBCourse(course_abr, course_name, section, number, reserved_cap, class_notes, class_type, days, time, open_seats,
-                       location, instructor, comment))
+                CSULBCourse(course_abr, course_name, section, number, reserved_cap, class_notes, class_type, days, time,
+                            open_seats, location, instructor, comment))
+    return courses
 
+
+def main():
+    courses = scrape_data_to_file()
     # Print each course
-
     user_course = input("Enter course to check open sections: ")
     for course in courses:
         user_course.upper()
-        if course._course_abr == user_course and course._open_seats == 'Open':
+        if course.course_abr == user_course and course.open_seats is True:
             print(course)
-main()
+
+
+if __name__ == "__main__":
+    main()
