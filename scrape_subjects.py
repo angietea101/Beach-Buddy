799a354763f2bef7a18a7564ce54edebd8cd300b
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2024-04-24 01:18:09 -0700

    FEAT: created a function to scrape fall courses
    folder path ~ Seasons/fall_2024, Seasons/summer_2024

--- scrape_subjects.py
+++ scrape_subjects.py
@@ -1,26 +1,35 @@
 import html_scraper
 import os.path
-
+import time
 file_name = "subjects.txt"
 
-with open(file_name, 'r') as file:
-    links = []
-    file_paths = []
-    for line in file:
-        data = line.strip().split(', ')
-        # gets rid of  - in subject names in order to name the files properly
-        course_name = data[0].replace('-', '')
-        course_abr = data[1]
+def scrape_fall():
+    with open(file_name, 'r') as file:
+        links = []
+        file_paths = []
+        for line in file:
+            data = line.strip().split(', ')
+            # gets rid of  - in subject names in order to name the files properly
+            course_name = data[0].replace('-', '')
+            course_abr = data[1]
 
-        # link to request html
-        subject_html = "http://web.csulb.edu/depts/enrollment/registration/class_schedule/Fall_2024/By_Subject/" + course_abr + ".html"
-        # os path to the folders
-        path = os.path.join("Seasons/fall_2024", course_name.replace(' ', '_') + "_scraped_data.txt")
+            # link to request html
+            subject_html = "http://web.csulb.edu/depts/enrollment/registration/class_schedule/Fall_2024/By_Subject/" + course_abr + ".html"
+            # os path to the folders
+            path = os.path.join("Seasons/fall_2024", course_name.replace(' ', '_') + "_scraped_data.txt")
 
-        # add the data to a list
-        links.append(subject_html)
-        file_paths.append(path)
+            # add the data to a list
+            links.append(subject_html)
+            file_paths.append(path)
 
-for i in range(len(links)):
-    html_scraper.write_data_to_file(file_paths[i], links[i])
+    for i in range(len(links)):
+        html_scraper.write_data_to_file(file_paths[i], links[i])
+def main():
+    scrape_fall()
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
+
 
