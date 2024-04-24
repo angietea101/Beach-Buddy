7bf2a941b7d0ec2fdd877f589bb6ca82a80dadf0
Angie <angietea101@gmail.com>
2024-04-24 02:17:49 -0700

    FEAT: Scrape summer 2024

--- scrape_subjects.py
+++ scrape_subjects.py
@@ -28,9 +28,33 @@ def scrape_fall():
     for i in range(len(links)):
         html_scraper.write_data_to_file(file_paths[i], links[i])
 
+def scrape_summer():
+    with open(file_name, 'r') as file:
+        links = []
+        file_paths = []
+        for line in file:
+            data = line.strip().split(', ')
+            # gets rid of  - in subject names in order to name the files properly
+            course_name = data[0].replace('-', '').lower()
+            course_abr = data[1]
+
+            # link to request html
+            subject_html = "http://web.csulb.edu/depts/enrollment/registration/class_schedule/Summer_2024/By_Subject/" \
+                           + course_abr + ".html"
+            # os path to the folders
+            path = os.path.join("seasons/summer_2024", course_name.replace(' ', '_') + "_scraped_data.txt")
+
+            # add the data to a list
+            links.append(subject_html)
+            file_paths.append(path)
+
+    for i in range(len(links)):
+        html_scraper.write_data_to_file(file_paths[i], links[i])
+
 
 def main():
     scrape_fall()
+    scrape_summer()
 
 
 if __name__ == "__main__":
