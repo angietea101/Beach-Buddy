e046678e044f89d73c50e5448b5bebc221ae5b33
Angie <angietea101@gmail.com>
2024-04-26 02:01:22 -0700

    FEAT: search() takes in abbreviation and checks if valid

--- main.py
+++ main.py
@@ -8,39 +8,43 @@ from discord import app_commands
 from discord.ext import commands
 from utils import csv_to_dictionary
 from config import BOT_TOKEN
+from typing import Literal
 
-bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+fall_path = "seasons/fall_2024"
+summer_path = "seasons/summer_2024"
+subjects_csv = "subjects.csv"
+subjects_abbreviation = csv_to_dictionary(subjects_csv)
+subjects_keys_list = list(subjects_abbreviation.keys())
 
 
 @bot.event
 async def on_ready():
     print("Beach Buddy is awake!")
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)")
-    except Exception as e:
-        print(e)
 
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f"Hello {ctx.author.mention}!")
+@bot.hybrid_command()
+async def sync(ctx: commands.Context):
+    await ctx.send("Syncing...")
+    await bot.tree.sync()
+    await ctx.send("Synced successfully")
 
 
-@bot.tree.command(name="search")
-@app_commands.describe(course="What is the course abbreviation + code? [EX: MATH 100]")
-async def search(interaction: discord.Interaction, course: str):
-    await interaction.response.send_message(f"Hey {interaction.user.mention}! You requested for {course}.")
+@bot.hybrid_command()
+async def ping(ctx, test: Literal['PONG', 'PANG']):
+    await ctx.send(f"{test}")
 
 
-def main():
-    bot.run(BOT_TOKEN)
-    # fall_path = "seasons/fall_2024"
-    # summer_path = "seasons/summer_2024"
-    # subjects_csv = "subjects.csv"
-    # subjects_abbreviation = csv_to_dictionary(subjects_csv)
-    # print(subjects_abbreviation)
+@bot.hybrid_command()
+async def search(ctx: commands.Context, abbreviation: str, code: int):
+    abbreviation = abbreviation.upper()
+    if abbreviation not in subjects_abbreviation:
+        await ctx.send(f"Invalid abbreviation. Examples: MATH, CECS, or BIOL")
+        return
+    else:
+        await ctx.send(f"Valid abbreviation.")
 
 
-if __name__ == "__main__":
-    main()
+bot.run(BOT_TOKEN)
