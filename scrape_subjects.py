e25911abe21473af6efa8ce4839063caf6abae21
Angie <angietea101@gmail.com>
2024-04-26 03:58:05 -0700

    FIX: Turn summer scraped data into csv files

--- scrape_subjects.py
+++ scrape_subjects.py
@@ -42,7 +42,7 @@ def scrape_summer(subjects_file):
             subject_html = "http://web.csulb.edu/depts/enrollment/registration/class_schedule/Summer_2024/By_Subject/" \
                            + course_abr + ".html"
             # os path to the folders
-            path = os.path.join("seasons/summer_2024", course_name.replace(' ', '_') + "_scraped_data.txt")
+            path = os.path.join("seasons/summer_2024", course_name.replace(' ', '_') + "_scraped_data.csv")
 
             # add the data to a list
             links.append(subject_html)
