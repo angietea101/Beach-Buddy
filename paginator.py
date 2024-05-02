abe43e39b2c38a4920ffd739715575bde86a999e
Angie <81064737+angietea101@users.noreply.github.com>
2024-05-01 18:45:24 -0700

    FEAT: Make page go in order by course section
    close #11

--- paginator.py
+++ paginator.py
@@ -1,29 +1,42 @@
 import discord
-from discord.ext import commands
 from typing import List
-from collections import deque
 
 
 class PaginatorView(discord.ui.View):
     def __init__(self, embeds: List[discord.Embed]) -> None:
-        super().__init__(timeout=30)
-
+        super().__init__(timeout=60)
         self._embeds = embeds
-        self._queue = deque(embeds)
         self._initial = embeds[0]
-        self._len = len(embeds)
         self._current_page = 1
+        self._max_page = len(embeds)
+        self._initial.set_footer(text=f"Page {self._current_page}/{self._max_page}")
+        # Disables the previous button at initial page
         self.children[0].disabled = True
-        self._queue[0].set_footer(text=f"Page of {self._current_page}/{self._len}")
+
+    def get_current_embed(self):
+        return self._embeds[self._current_page - 1]
+
+    def previous_page(self):
+        if self._current_page > 1:
+            self._current_page -= 1
+        return self.get_current_embed()
+
+    def next_page(self):
+        if self._current_page < self._max_page:
+            self._current_page += 1
+        return self.get_current_embed()
 
     async def update_buttons(self, interaction: discord.Interaction) -> None:
-        for index in self._queue:
-            index.set_footer(text=f"Page of {self._current_page}/{self._len}")
-        if self._current_page == self._len:
+        for index in self._embeds:
+            index.set_footer(text=f"Page {self._current_page}/{self._max_page}")
+
+        # Disables the next button if at max page
+        if self._current_page == self._max_page:
             self.children[1].disabled = True
         else:
             self.children[1].disabled = False
 
+        # Disables the previous button if at first page
         if self._current_page == 1:
             self.children[0].disabled = True
         else:
@@ -33,20 +46,12 @@ class PaginatorView(discord.ui.View):
 
     @discord.ui.button(style=discord.ButtonStyle.danger, label="PREVIOUS")
     async def previous(self, interaction: discord.Interaction, _):
-        self._queue.rotate(-1)
-        embed = self._queue[0]
-        self._current_page -= 1
+        embed = self.previous_page()
         await self.update_buttons(interaction)
-        await interaction.response.edit_message(embed=embed)
+        await interaction.response.edit_message(embed=embed, view=self)
 
     @discord.ui.button(style=discord.ButtonStyle.success, label="NEXT")
     async def next(self, interaction: discord.Interaction, _):
-        self._queue.rotate(1)
-        embed = self._queue[0]
-        self._current_page += 1
+        embed = self.next_page()
         await self.update_buttons(interaction)
-        await interaction.response.edit_message(embed=embed)
-
-    @property
-    def initial(self) -> discord.Embed:
-        return self._initial
+        await interaction.response.edit_message(embed=embed, view=self)
