882f8e3131714cf7a41f6908d43303251da46151
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2024-05-01 23:49:21 -0700

    WIP: notification for schedule update
    uses discord task.loop in order to get the time every second and run scheduled_scrape and send a message to a specified channel at 05:03:00 everyday. Also added a command for users to specify the notifcation channel

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ Author: Angie Tran and Diego Cid
 Description: Main function to run our script
 """
 import discord
-from discord.ext import commands
-from utils import csv_to_dictionary, get_csv_path, get_course_codes, get_class_infos, create_embed
+from discord.ext import commands, tasks
+from utils import csv_to_dictionary, get_csv_path, get_course_codes, get_class_infos, create_embed, get_time, save_notif_channel
 from config import BOT_TOKEN
 from typing import Literal
 from paginator import PaginatorView
-from datetime import datetime
 from scrape_subjects import scrape_fall
 from scrape_subjects import scrape_summer
-import threading
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="/", intents=intents)
@@ -22,21 +20,17 @@ subjects_abbreviation = csv_to_dictionary(subjects_csv)
 
 
 def scheduled_scrape():
-    threading.Timer(1, scheduled_scrape).start()
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    if current_time == '05:03:30':
-        print("Scraping...")
-        subjects_file = "subjects.csv"
-        scrape_fall(subjects_file)
-        scrape_summer(subjects_file)
-        print("Complete")
+    print("Scraping...")
+    subjects_file = "subjects.csv"
+    scrape_fall(subjects_file)
+    scrape_summer(subjects_file)
+    print("Complete")
 
 
 @bot.event
 async def on_ready():
     print("Beach Buddy is awake!")
-    scheduled_scrape()
+    notify_scrape.start()
 
 
 @bot.hybrid_command()
@@ -50,7 +44,28 @@ async def sync(ctx: commands.Context):
 async def ping(ctx, test: Literal['PONG', 'PANG']):
     await ctx.send(f"{test}")
 
+@bot.hybrid_command()
+async def notify(ctx, channel: discord.TextChannel):
+    channel_id = channel.id
+    guild_id = ctx.guild.id
+    save_notif_channel(guild_id, channel_id)
+    await ctx.send(f"Notification channel set to {channel.mention}")
 
+@tasks.loop(seconds = 1)
+async def notify_scrape():
+    current_time = get_time()
+    if current_time == '05:03:30':
+        scheduled_scrape()
+        try:
+            with open('notif.txt', 'r') as file:
+                for line in file:
+                    data = line.strip().split(',')
+                    guild = bot.get_guild(int(data[0]))
+                    if guild:
+                        channel = guild.get_channel(int(data[1]))
+                        await channel.send("Schedule Updated")
+        except Exception as e:
+            print(f"An error occured: {e}")
 @bot.hybrid_command()
 async def search(ctx: commands.Context, season: Literal["Fall 2024", "Summer 2024"], abbreviation: str, code: str,
                  opened_only: Literal["True", "False"]):
