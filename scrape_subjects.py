3ee005e8b4105ee8be25bd30a2608b0773f1e3a8
Angie <angietea101@gmail.com>
2024-05-28 03:00:03 -0700

    FEAT: Scrape subjects into one long csv file based on season

--- scrape_subjects.py
+++ scrape_subjects.py
@@ -15,58 +15,46 @@
 # You should have received a copy of the MIT License
 # along with Beach Buddy. If not, see <https://mit-license.org/>.
 
-import html_scraper
+from html_scraper import *
 import os.path
 import csv
 import time
 
 
 def scrape_fall(subjects_file):
+    csv_file = "fall_2024.csv"
+    file_path = f"seasons/{csv_file}"
+    # Deletes the csv to create a new one to append to
+    delete_csv_file(file_path)
     with open(subjects_file, 'r') as file:
-        links = []
-        file_paths = []
         for line in file:
             data = line.strip().split(', ')
-            # gets rid of  - in subject names in order to name the files properly
-            course_name = data[0].replace('-', '').lower()
+            # retrieve course abbreviations
             course_abr = data[1]
 
             # link to request html
             subject_html = "http://web.csulb.edu/depts/enrollment/registration/class_schedule/Fall_2024/By_Subject/" \
                            + course_abr + ".html"
-            # os path to the folders
-            path = os.path.join("seasons/fall_2024", course_name.replace(' ', '_') + "_scraped_data.csv")
 
-            # add the data to a list
-            links.append(subject_html)
-            file_paths.append(path)
-
-    for i in range(len(links)):
-        html_scraper.write_data_to_file(file_paths[i], links[i])
+            write_data_to_file(file_path, subject_html)
 
 
 def scrape_summer(subjects_file):
+    csv_file = "summer_2024.csv"
+    file_path = f"seasons/{csv_file}"
+    # Deletes the csv to create a new one to append to
+    delete_csv_file(file_path)
     with open(subjects_file, 'r') as file:
-        links = []
-        file_paths = []
         for line in file:
             data = line.strip().split(', ')
-            # gets rid of  - in subject names in order to name the files properly
-            course_name = data[0].replace('-', '').lower()
+            # retrieve course abbreviations
             course_abr = data[1]
 
             # link to request html
             subject_html = "http://web.csulb.edu/depts/enrollment/registration/class_schedule/Summer_2024/By_Subject/" \
                            + course_abr + ".html"
-            # os path to the folders
-            path = os.path.join("seasons/summer_2024", course_name.replace(' ', '_') + "_scraped_data.csv")
 
-            # add the data to a list
-            links.append(subject_html)
-            file_paths.append(path)
-
-    for i in range(len(links)):
-        html_scraper.write_data_to_file(file_paths[i], links[i])
+            write_data_to_file(file_path, subject_html)
 
 
 def main():
