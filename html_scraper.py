d117e90bc82bc488a213e04db1ef71a56970b471
Angie <angietea101@gmail.com>
2024-05-28 03:16:51 -0700

    FIX: Class notes writing as html instead of text

--- html_scraper.py
+++ html_scraper.py
@@ -43,7 +43,7 @@ def write_data_to_file(csv_file_path, url):
         for course_block in course_blocks:
             course_code = course_block.find('span', class_='courseCode').text
             course_title = course_block.find('span', class_='courseTitle').text
-            course_title = course_title.replace(',', ' ')
+            course_title = course_title.replace(',', '')
             units = course_block.find('span', class_='units').text
 
             # Find all tables within the current course block
@@ -75,23 +75,29 @@ def write_data_to_file(csv_file_path, url):
                             full_text = ""
                             text = cell.contents
                             for item in text:
-                                item = str(item)
-                                if "<br/>" not in item:
-                                    text_stripped = item.strip()
+                                item_string = str(item)
+                                if "href=" in item_string:
+                                    text_replaced = cell.text.replace(',', ' ')
+                                    full_text += text_replaced.strip()
+                                elif "<br/>" not in item_string:
+                                    text_stripped = item_string.strip()
                                     text_replaced = text_stripped.replace('\n', ' ')
                                     text_replaced = text_replaced.replace(',', ' ')
                                     full_text += f" {text_replaced}"
-                            row_data.append(full_text[1:])
+                                    full_text = full_text[1:]
+                            row_data.append(full_text)
                     # Write the row data into the file with comma-separated values
                     if len(row_data) > 3:
                         file.write(', '.join(row_data) + '\n')
 
 
 def main():
-    subject = input("Subject abbreviation: ").upper()
+    # subject = input("Subject abbreviation: ").upper()
+    subject = "WGSS"
     url = 'http://web.csulb.edu/depts/enrollment/registration/class_schedule/Fall_2024/By_Subject/' + subject + '.html'
     response = requests.get(url)
     file_name = str(f"{subject.lower()}_scraped_data.csv")
+    delete_csv_file("wgss_scraped_data.csv")
     write_data_to_file(file_name, url)
 
 
